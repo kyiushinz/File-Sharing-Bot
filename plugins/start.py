014608b45b813d37eb1cda4d10206b2accc98862
kyiushinz <84819080+kyiushinz@users.noreply.github.com>
2021-07-02 04:41:13 +0700

    fork

--- plugins/start.py
+++ plugins/start.py
@@ -73,8 +73,8 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("😊 About Me", callback_data = "about"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close")
+                    InlineKeyboardButton("😊 Randovlsky", callback_data = "about"),
+                    InlineKeyboardButton("🔒 Tutup", callback_data = "close")
                 ]
             ]
         )
@@ -94,7 +94,7 @@ async def start_command(client: Client, message: Message):
 
 @Bot.on_message(filters.command('start') & filters.private)
 async def not_joined(client: Client, message: Message):
-    text = "<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>"
+    text = "<b>KALO MAU NONTON SUBSCRIBE DULU GOBLOG😁\n\nKindly Silakan Join Channel dulu bro.</b>"
     message_text = message.text
     try:
         command, argument = message_text.split()
