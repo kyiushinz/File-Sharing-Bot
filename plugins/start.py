4719fcb51b888b67d36344152e4e39d6cca6a325
kyiushinz <84819080+kyiushinz@users.noreply.github.com>
2021-07-02 05:55:30 +0700

    fixed 1

--- plugins/start.py
+++ plugins/start.py
@@ -94,14 +94,14 @@ async def start_command(client: Client, message: Message):
 
 @Bot.on_message(filters.command('start') & filters.private)
 async def not_joined(client: Client, message: Message):
-    text = "<b>KALO MAU NONTON SUBSCRIBE DULU. Owner : @Randovlsky\n\nGausah pelit Subscribe Goblog 🙄\n\nGrup Cari Teman - @grup_cari_teman</b>"
+    text = "<b>⚠️ MAU NONTON SUBSCRIBE DULU.\n\n🛑Contact : @Randovlsky\n\nTerimakasih atas dukungan kalian...</b>"
     message_text = message.text
     try:
         command, argument = message_text.split()
         text = text + f" <b>and <a href='https://t.me/{client.username}?start={argument}'>try again</a></b>"
     except ValueError:
         pass
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("👉 SUBSCRIBE GOBLOG 🔓", url = client.invitelink)]])
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("SUBSCRIBE 🔓", url = client.invitelink)]])
     await message.reply(
         text = text,
         reply_markup = reply_markup,
